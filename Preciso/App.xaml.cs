fbae14f1d6a46e7246c01d0a110103089c9727ec
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-01-26 16:37:34 -0300

    Preciso App

---
+++ Preciso/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Preciso.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Preciso
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new StartPageView());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
