3576f9b163c7a53e78fbc70de63560b673fd2c55
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-01-29 12:36:06 -0300

    pagina de login

---
+++ Preciso/Views/LoginPageView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Preciso.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPageView : ContentPage
+    {
+        public LoginPageView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTapGestureRecognizerTapped(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
