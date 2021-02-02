1bf01a707d0e139e486279a6fbcd20e965838ad6
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-02-02 17:07:47 -0300

    View model TipoProfissional

--- Preciso/ViewModels/TipoProfissionalViewModel.cs
+++ Preciso/ViewModels/TipoProfissionalViewModel.cs
@@ -1,12 +1,28 @@
-﻿using Preciso.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Preciso.Models;
+using Preciso.ViewModels;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace Preciso.Profissional.ViewModels
 {
     public class TipoProfissionalViewModel : BaseViewModel
     {
+        private ObservableCollection<TipoProfissional> _tiposProfissionais;
+        public ObservableCollection<TipoProfissional> TiposProfissionais
+        {
+            get => _tiposProfissionais;
+            set => SetProperty(ref _tiposProfissionais, value);
+        }
 
+        public TipoProfissionalViewModel()
+        {
+            ListaTiposProfssionais();
+        }
+
+        public async Task<ObservableCollection<TipoProfissional>> ListaTiposProfssionais()
+        {
+            //TODO - Fazer API e camada de DataBase.
+            return TiposProfissionais;
+        }
     }
 }
