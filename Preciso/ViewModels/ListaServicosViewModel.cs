5db5bf8c6856cbda38995b75e342320a83c91730
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-02-08 16:42:18 -0300

    Lista servicos ok

---
+++ Preciso/ViewModels/ListaServicosViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Preciso.Data;
+using Preciso.Models;
+using System.Collections.ObjectModel;
+
+namespace Preciso.ViewModels
+{
+    public class ListaServicosViewModel : BaseViewModel
+    {
+        private readonly FirebaseService firebase;
+
+        public ObservableCollection<Servico> Servicos { get; set; }
+
+        public ListaServicosViewModel()
+        {
+            firebase = new FirebaseService();
+            Servicos = CarregarServicos();            
+        }
+
+        private ObservableCollection<Servico> CarregarServicos()
+        {
+            return firebase.ListaServicos();
+        }
+    }
+}
