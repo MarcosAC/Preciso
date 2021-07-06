9a4ba8286d78a8e1d20c7f68b13368cc316b9828
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-07-06 17:37:29 -0300

    Model Cliente

---
+++ Preciso.Cliente/Preciso.Cliente/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Preciso.Cliente.Models
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Celular { get; set; }
+        public string Email { get; set; }
+    }
+}
