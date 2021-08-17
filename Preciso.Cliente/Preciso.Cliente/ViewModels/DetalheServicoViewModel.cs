fe1ad7fdb5909c8c0bb40c3a1a93f8535a467613
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-08-17 11:13:47 -0300

    Pagina lista e detalhe de servicos

---
+++ Preciso.Cliente/Preciso.Cliente/ViewModels/DetalheServicoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Preciso.Cliente.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Preciso.Cliente.ViewModels
+{
+    public class DetalheServicoViewModel
+    {
+        public DetalheServicoViewModel(Servico servicoSelecionado)
+        {
+
+        }
+    }
+}
---
+++ Preciso.Cliente/Preciso.Cliente/ViewModels/ListaServicosViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Preciso.Cliente.Data;
+using Preciso.Cliente.Models;
+using Preciso.Cliente.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Preciso.Cliente.ViewModels
+{
+    public class ListaServicosViewModel
+    {
+        private readonly ServicoService servicoService;
+
+        public ObservableCollection<Servico> Servicos { get; set; }
+
+        public ListaServicosViewModel()
+        {
+            servicoService = new ServicoService();
+            Servicos = CarregarServicos();
+        }
+
+        private ObservableCollection<Servico> CarregarServicos()
+        {
+            return servicoService.ListaServicos();
+        }
+
+        private Command _selecionarServicoCommand;
+        public Command SelecionarServicoCommand =>
+            _selecionarServicoCommand ?? (_selecionarServicoCommand = new Command<Servico>(async servico => await ExecuteSelecionarServicoCommand(servico)));
+
+        private async Task ExecuteSelecionarServicoCommand(Servico servicoSelecionado)
+        {
+            if (servicoSelecionado == null)
+                return;
+
+            await App.Current.MainPage.Navigation.PushAsync(new DetalheServicoView(servicoSelecionado));
+        }
+    }
+}
