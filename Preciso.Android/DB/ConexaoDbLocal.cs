b2fe92e2e1610f8539e85ac7c89a4201ba3068c1
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-11-19 17:00:30 -0300

    Configuracoes banco de dados local

---
+++ Preciso.Android/DB/ConexaoDbLocal.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Preciso.Data.LocalData;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Preciso.Droid.DB
+{
+    public class ConexaoDbLocal : IConexaoDbLocal
+    {
+        public string Conexao(string nomeDb)
+        {
+            string stringConexao = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string bancoDadosLocal = Path.Combine(stringConexao, nomeDb);
+
+            return bancoDadosLocal;
+        }
+    }
+}
---
+++ Preciso/Data/LocalData/DbLocal.cs
@@ -0,0 +1,23 @@
+﻿using Preciso.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Preciso.Data.LocalData
+{
+    public class DbLocal
+    {
+        private static SQLiteConnection _conexao;
+
+        public DbLocal()
+        {
+            var dependencyService = DependencyService.Get<IConexaoDbLocal>();
+            string stringConexao = dependencyService.Conexao("dbLocal.sqlite");
+
+            _conexao = new SQLiteConnection(stringConexao);
+            _conexao.CreateTable<Profissional>();
+        }
+    }
+}
